303cd361fc9286b51be5957b00a8a9d34b43c088
Victor <vb@live.fr>
2020-01-22 20:09:13 +0100

    Complete the config

--- docs/conf.py
+++ docs/conf.py
@@ -9,10 +9,9 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('..'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -30,17 +29,14 @@ release = '0.1.5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-# extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
-extensions = []
-# autodoc settings
-# autodoc_mock_imports = ['numpy', 'bokeh', 'seaborn', 'pandas']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+autodoc_mock_imports = ['numpy', 'bokeh', 'seaborn', 'pandas']
 
 # napoleon settings
-# napoleon_google_docstring = True
-# napoleon_numpy_docstring = False
+napoleon_google_docstring = True
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
+templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
